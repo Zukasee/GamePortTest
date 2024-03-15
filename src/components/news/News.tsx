f4fed7c5e5432ebfe8ca7fc7d6e2661e842024b9
Artem <fdggfh@gmail.com>
2024-03-16 01:38:06 +0300

    fonts

--- src/components/news/News.tsx
+++ src/components/news/News.tsx
@@ -1,35 +1,40 @@
 import React from "react";
 import OneNews from "./oneNews/OneNews";
+import first from "../../fonts/forNews/freeHour2.jpg";
+import friend from "../../fonts/forNews/frind.jpg";
+import sale from "../../fonts/forNews/sale.png";
+import dota from "../../fonts/forNews/freeHour.jpg";
+import cs from "../../fonts/forNews/cs.png";
 
 const news = [
     {
         name: 'Первый заказ',
-        img: '',
-        text: '',
+        img: first,
+        text: 'sfdggsakd',
         timePost: ''
     },
     {
         name: 'Приведи друга',
-        img: '',
-        text: '',
+        img: friend,
+        text: 'dkjfdsl',
         timePost: ''
     },
     {
         name: 'Накопительная система',
-        img: '',
-        text: '',
+        img: sale,
+        text: 'dsfjkdjksf',
         timePost: ''
     },
     {
         name: 'Турнир по Dota2',
-        img: '',
-        text: '',
+        img: dota,
+        text: 'dsjfhksd',
         timePost: ''
     },
     {
         name: 'Турнир по Source2',
-        img: '',
-        text: '',
+        img: cs,
+        text: 'dsklcz;ck',
         timePost: ''
     },
 ]
@@ -38,7 +43,7 @@ const News = () => {
     return (
         <>
             {news.map((item) => (
-                <OneNews name={item.name}/>
+                <OneNews name={item.name} img={item.img}/>
             ))}
         </>
     )
--- src/components/news/oneNews/OneNews.tsx
+++ src/components/news/oneNews/OneNews.tsx
@@ -2,15 +2,17 @@ import React, { FC } from "react";
 import s from './OneNews.module.css';
 
 interface OneNewsProps {
-    name: string
+    name?: string,
+    text?: string,
+    img?: string,
 }
 
 const OneNews: FC<OneNewsProps> = (props) => {
     return (
         <div className={s.newsDiv}>
             <div className={s.border}>
-                <div className={s.img}>
-                    <p>{props?.name}</p>
+                <div className={s.img} >
+                   <img src={props.img} alt={props.name}/>
                 </div>
             </div>
         </div>
